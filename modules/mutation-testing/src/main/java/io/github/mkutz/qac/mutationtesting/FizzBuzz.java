aa557a47d21f3fb90ea5d21a6aa1434b6ff5d930
Michael Kutz <michael.kutz@rewe-group.com>
2024-07-19 12:50:04 +0200

    Make FizzBuzz static

--- modules/mutation-testing/src/main/java/io/github/mkutz/qac/mutationtesting/FizzBuzz.java
+++ modules/mutation-testing/src/main/java/io/github/mkutz/qac/mutationtesting/FizzBuzz.java
@@ -2,7 +2,7 @@ package io.github.mkutz.qac.mutationtesting;
 
 public class FizzBuzz {
 
-  public String fizzbuzz(int i) {
+  public static String fizzbuzz(int i) {
     if (i % 15 == 0) return "FizzBuzz";
     if (i % 3 == 0) return "Fizz";
     if (i % 5 == 0) return "Buzz";
--- modules/mutation-testing/src/test/java/io/github/mkutz/qac/mutationtesting/FizzBuzzTest.java
+++ modules/mutation-testing/src/test/java/io/github/mkutz/qac/mutationtesting/FizzBuzzTest.java
@@ -1,25 +1,24 @@
 package io.github.mkutz.qac.mutationtesting;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import org.junit.jupiter.api.Test;
+
+import static io.github.mkutz.qac.mutationtesting.FizzBuzz.fizzbuzz;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class FizzBuzzTest {
 
-  FizzBuzz fizzBuzz = new FizzBuzz();
-
   @Test
   void fizzbuzz_1() {
-    assertThat(fizzBuzz.fizzbuzz(1)).isEqualTo("1");
+    assertThat(fizzbuzz(1)).isEqualTo("1");
   }
 
   @Test
   void fizzbuzz_3() {
-    assertThat(fizzBuzz.fizzbuzz(3)).isEqualTo("Fizz");
+    assertThat(fizzbuzz(3)).isEqualTo("Fizz");
   }
 
   @Test
   void fizzbuzz_5() {
-    assertThat(fizzBuzz.fizzbuzz(5)).isEqualTo("Buzz");
+    assertThat(fizzbuzz(5)).isEqualTo("Buzz");
   }
 }
