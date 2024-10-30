bdc52d9e0776303be7615eaabf868ab42f7e0ecc
Janina Nemec <janina.nemec@rewe-digital.com>
2024-10-30 15:31:36 +0100

    [approval-testing] add some more variants

--- modules/approval-testing/src/test/java/io/github/mkutz/qac/approvaltesting/FileComparisonTest.java
+++ modules/approval-testing/src/test/java/io/github/mkutz/qac/approvaltesting/FileComparisonTest.java
@@ -1,30 +1,29 @@
 package io.github.mkutz.qac.approvaltesting;
 
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static io.github.mkutz.qac.approvaltesting.FakeFunctionalityKt.anOrderWasProcessed;
 import static io.github.mkutz.qac.approvaltesting.FakeFunctionalityKt.callRestEndpoint;
 import static io.github.mkutz.qac.approvaltesting.TestOrderBuilderKt.aDefaultOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
+
 class FileComparisonTest {
 
-    private String TEST_DIR = "src/test/resources/json/FileComparisonTest/";
+  private String TEST_DIR = "src/test/resources/json/FileComparisonTest/";
 
-    @Test
-    void assertionTest() throws IOException {
-        String orderId = "someOrderId";
-        ShopOrder order = aDefaultOrder(orderId);
+  @Test
+  void assertionTest() throws IOException {
+    String orderId = "someOrderId";
+    ShopOrder order = aDefaultOrder(orderId);
 
-        anOrderWasProcessed(order);
+    anOrderWasProcessed(order);
 
-        String result = callRestEndpoint(orderId);
+    String result = callRestEndpoint(orderId);
 
-        String expected = new String(Files.readAllBytes(Paths.get(TEST_DIR + "expectedOrder.json")));
-        assertThat(result).isEqualToIgnoringWhitespace(expected);
-    }
+    String expected = new String(Files.readAllBytes(Paths.get(TEST_DIR + "expectedOrder.json")));
+    assertThat(result).isEqualToIgnoringWhitespace(expected);
+  }
 }
